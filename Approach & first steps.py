f0858b0caffe7e9d1300644534ed4f2a08543b93
Annikw <107843525+Annikw@users.noreply.github.com>
2022-06-21 09:06:37 +0200

    Update Approach & first steps.py

--- Approach & first steps.py
+++ Approach & first steps.py
@@ -5,8 +5,8 @@
 #manually generated API https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple
 
 #simple creating an API manually to test
-from tkinter import MULTIPLE
-from unicodedata import category
+#from tkinter import MULTIPLE
+#from unicodedata import category
 import requests
 response = requests.get("https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple")
 print(response.headers)
@@ -14,7 +14,6 @@ print(response.headers)
 #next would be automating the request based on how many function in which difficulty level etc we want
 
 #CODE
-
 #DATA CONNECTION
 
 #Creating the base class 'Question' 
@@ -39,31 +38,8 @@ class Questions:
 
 #Creating the classes 'easy', 'medium' and 'difficult question' which inherit the base class
 #we are creating a list for each object of the class --> so that it can later be stores in the question data base
- 
-
- #we could do inheritance for easy medium and difficult question but I dont think it is necessary really
- class Easy_question(Question):
-    """
-    The Easy_question class inherits the Question class and contains information about the object only relevant for the easy questions
-
-    Attributes
-    ----------
-    XXX: todo
-     ...
-    """
-    def __repr__(self):
-        return ",".join([str(self.question), str(self.correct_answer),str(self.wrong_answer), str(self.category), str(self.multiple) ])
-    def __init__(self, question: str, correct_answer: str, wrong_answer: str, category: str, multiple: str) -> None:
-        super().__init__(question, correct_answer, wrong_answer, category, multiple)
-    
 
 class Quiz:
-    #here is where we actually mix and match the questions together to form a quiz which users can take
-    #I copied in my code from the warehousing so that we see how functions and the way the information is saved could look like.
-    #An idea would be to save all the questions of the quiz in a list. See below:
-
-
-class Warehouse:
     """
     The warehouse class provides functionalities of stock count; stock removal & add-in and stock transfer.
 
@@ -78,52 +54,32 @@ class Warehouse:
     blouse_stock: 
       Takes objects of the class 'Blouse' as input. It is a list of Blouse[s].
     """
-    def __init__(self, location: str = 'Europe', shirt_stock: list = [], pants_stock: list = [], blouse_stock: list = []):
-        self.location = location
-        self.shirt_stock = shirt_stock
-        self.pants_stock = pants_stock
-        self.blouse_stock = blouse_stock
+    #here is where we actually mix and match the questions together to form a quiz which users can take
+    #I copied in my code from the warehousing so that we see how functions and the way the information is saved could look like.
+    #An idea would be to save all the questions of the quiz in a list. See below:
 
-#Print stock count functions
-    def print_stock_count_shirt(self):
-        length = len(self.shirt_stock)
-        print(f"There is {length} shirt[s] in the warehouse")
-    
-    def print_stock_count_pants(self):
-        length = len(self.pants_stock)
-        print(f"There is {length} pants in the warehouse")
+    def __init__(self, category: str = 'Entertainment: Books', questions_easy: list = [], questions_medium: list = [], questions_difficult: list = []):
+        self.category = category
+        self.questions_easy = questions_easy
+        self.questions_medium = questions_medium
+        self.questions_difficult = questions_difficult
 
-    def print_stock_count_blouse(self):
-        length = len(self.blouse_stock)
-        print(f"There is {length} blouse[s] in the warehouse")
+#Get Question details functions
+    def get_questions_easy(self):
+        return self.questions_easy
 
-#Get stock functions
-    def get_stock_shirt(self):
-        return self.shirt_stock
+    def get_questions_medium(self):
+        return self.questions_medium
 
-    def get_stock_pants(self):
-        return self.pants_stock
-
-    def get_stock_blouse(self):
-        return self.blouse_stock
+    def get_questions_difficult(self):
+        return self.questions_difficult
 
 #Add stock functions
-#TODO in all following: use descriptive variable names instead of x
-    def add_stock_shirt(self, shirt_input: Shirt):
-        self.shirt_stock.append(shirt_input)
+    def add_questions_easy(self, easy_input: questions_easy):
+        self.questions_easy.append(easy_input)
 
-    def add_stock_pants(self, pants_input: Pants):
-        self.pants_stock.append(pants_input)
-    
-    def add_stock_blouse(self, blouse_input: Blouse):
-        self.blouse_stock.append(blouse_input)
+    def add_questions_medium(self, medium_input: questions_medium):
+        self.questions_medium.append(medium_input)
 
-#Update/ take out stock functions
-    def remove_stock_shirt(self, shirt_input: Shirt):
-        self.shirt_stock.remove(shirt_input)
-
-    def remove_stock_pants(self, pants_input: Pants):
-        self.pants_stock.remove(pants_input)
-    
-    def remove_stock_blouse(self, blouse_input: Blouse):
-        self.blouse_stock.remove(blouse_input)
+    def add_questions_difficult(self, difficult_input: questions_difficult):
+        self.questions_difficult.append(difficult_input)
