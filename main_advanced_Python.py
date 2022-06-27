c1b07e5322575bfbeb2b5b604799ce127419e3d3
LukasKev <kevin.krkoska@gmail.com>
2022-06-27 10:37:46 +0200

    Adding notes & further todos

--- main_advanced_Python.py
+++ main_advanced_Python.py
@@ -31,6 +31,14 @@ from unicodedata import category
 import requests
 import random
 
+# NOTE: Standardize formatting of comments
+# NOTE: Move classes to separate files
+# NOTE: Wrap code in a function and call it using if _name_ == '_main_'
+# NOTE: Use functions to encapsulate concepts. 
+#       e.g. instead of comments such as "Retrieving data from API based on user's choices" create a function called 'retrieve_questions', ...
+
+
+
 #----------------Creating dictionaries for Question settings-------------------------
 '''the dictionaries will allow the user to choose the different categories'''
 
@@ -41,15 +49,6 @@ Categories = {0 : "Random", 9 : 'General Knowledge', 10 : 'Entertainment: Books'
         23 : 'History', 24 : 'Politics', 25 : 'Art', 26:'Celebrities', 27 : 'Animals', 28 : 'Vehicles', 29 :  'Entertainment: Comics',
         30 : 'Science: Gadgets', 31: 'Entertainment: Japanese Anime & Manga', 32 : 'Entertainment: Cartoon & Animations'}
 
-#TODO delete(?)
-'''
-#Dictionary for the difficulty
-Difficulty = {1: "easy", 2: "medium", 3: "hard"}
-
-#Dictionary for the type: Allows the user to type 1 or 2, and will user the other dict to format the answer for the URL
-Type1 = {1 : "True/False", 2 : "Multiple"}
-Type = {"True/False" : "boolean", "Multiple" : "multiple"}
-'''
 
 #----------------Designing the User interaction: choosing the category-------------------------
 '''In this section, we are asking the user to give input in terms of categories, difficulty, number of questions, and type of question to allow
@@ -66,10 +65,13 @@ print("Here is the Menu:")
 for i in Categories:
     print(i, Categories[i])
 
+# NOTE: can get rid of this variable and instead use a while true: with a break
 check_loop = True
 while check_loop:
     #Input Category
+    # NOTE: use snake case (lower case beginning)
     Chose_Cat = 0
+    # NOTE: validate that the user input is actually a number and recover if not
     Chosen_Cat = int(input("Please, input here a (valid) number for a category: "))
     #Check valid category
     try:
@@ -81,68 +83,23 @@ while check_loop:
 print('\n')
 
 #-----NB QUESTIONS-----
+# NOTE: better variable naming and move this all into a function
 NB_Questions = 0
 #Check valid input
 while NB_Questions < 1 or NB_Questions >50:
+    # NOTE: recover if not integer input
     NB_Questions = int(input('How many Questions ? (<30) '))
+    # NOTE: this should not be stored in the same variable as this is confusing. Keep the user`s choice and do the buffering somewhere else
     NB_Questions = NB_Questions + 20 #proposal here is to always add 20 questions more so that we can always adjust the difficulty level. --> Buffer
 print('\n')
 
-#comment: might no longer be needed? --> quizzes will customize the level of difficulty based on the user's performance
-#TODO delete when ready
-"""
-#------DIFFICULTY-------
-print('Great, which difficulty ? ')
-for i in Difficulty:
-    print(i, Difficulty[i])
-
-#Check valid input
-check_loop = True
-while check_loop:
-    Chosen_Diff = int(input("Please, input here a valid number for a difficulty: "))
-    try:
-        print('You have chosen :', Difficulty[Chosen_Diff])
-        check_loop = False
-    except:
-        check_loop = True
-
-print('\n')
-
-
-
-#-------TYPE------
-print('Finally, which type ? ')
-for i in Type1:
-    print(i, Type1[i])
-
-#Check valid input
-check_loop = True
-while check_loop:
-    Chosen_type = int(input("Please, input here a valid number for a difficulty: "))
-    try:
-        print('You have chosen :', Type[Type1[Chosen_type]])
-        check_loop = False
-    except:
-        check_loop = True
-
-
-print('\n')
-print("Your", str(Difficulty[Chosen_Diff]), str(NB_Questions), "quizz about", str(Categories[Chosen_Cat]), "is about to start.")
-"""
-
 #----------------Retrieving data from API based on user's choices-------------------------
 
-#this is the url with all the input factors. TODO delete?
-'''
-url = "https://opentdb.com/api.php?amount="+str(NB_Questions)+ "&category=" + str(Chosen_Cat) + "&difficulty=" + str(Difficulty[Chosen_Diff]) +"&type=" + str(Type[Type1[Chosen_type]])
-response = requests.get(url)'''
-
+# NOTE: what is this used for?
 Score = 0
 
-#Check that we have enough questions for the constrains
-#(Sometimes, the API doesn't generate any questions: e.g. Cat:27, Questions:40, Level: Hard, Type: Multiple)
-
 #url for choosing only category and number of questions
+# NOTE: abstract the API logic away behind a function or even create a `question_api` class to interact with
 url = "https://opentdb.com/api.php?amount="+str(NB_Questions)+ "&category=" + str(Chosen_Cat) + "&type=boolean"
 
 response = requests.get(url)
@@ -190,21 +147,24 @@ for i in response_json['results']:
 #with these instances of class Question, we can now create Quizzes.
     
 class Quiz:
-    def init(self, number_questions_total, q_list_easy, q_list_medium, q_list_hard):
+    def _init_(self, number_questions_total, q_list_easy, q_list_medium, q_list_hard):
         
         self.number_questions_total = number_questions_total
         self.question_score = 0
         
+        # NOTE: use a dictionary here (e.g. self.questions["easy"], ...)
         self.question_list_easy = q_list_easy #list of easy questions
         self.question_list_medium = q_list_medium
         self.question_list_hard = q_list_hard
     
+        # NOTE: use dictionary here, could even combine with above
         self.q_num_easy = 0 #counter for easy questions
         self.q_num_medium = 0
         self.q_num_hard = 0
         
         self.difficulty_level = 1 #1 for easy, 2 for medium and 3 for hard
         
+    # NOTE: if you use a dictionary it would be easy to combine all the following three methods into one and providing "easy"/"medium"/"difficult" as a key to the method 
     def next_question_easy(self):
         try:
             current_question = self.question_list_easy[self.q_num_easy]
@@ -214,6 +174,7 @@ class Quiz:
             if self.check_answer(user_answer, current_question.correct_answer):
                 self.difficulty_level = 2
         except IndexError:
+            # NOTE: if the level 2 represents medium than you could represent this using e.g. an enum to make it easy to read
             self.difficulty_level = 2 #redirect to medium difficulty if we don't have any easy questions in dataset
 
     def next_question_medium(self):
@@ -258,11 +219,13 @@ class Quiz:
             return False 
 
 #------------------------- Running the Quiz ---------------------------------
+# NOTE: shouldn't this be coming from the user input?
 number_questions_total = 5 #define how many questions we want to ask our user
 quiz = Quiz(number_questions_total, question_list_easy, question_list_medium, question_list_hard)
 
 
 while quiz.remaining_questions():
+    # NOTE: see above this could be a single method quiz.next_question as the state of difficulty level is already kept by quiz and does not need to be exposed here
     if quiz.difficulty_level == 1:
         quiz.next_question_easy()
     elif quiz.difficulty_level == 2:
@@ -272,64 +235,5 @@ while quiz.remaining_questions():
 
 print(f"Your final score is: {quiz.question_score}")
 
-
-
-
-
-
-
-
-#TODO no longer needed? delete when ready
-"""
-#--------------------------------Non OOP based approach-------------------------
-#Q from Annik: what happens if the number of questions is equal to the length of the response??
-
-if len(response.json()['results']) == 0:
-    print("Sorry, we don't have any question with these requirments .. ")
-    # --> When OOP, makes it redirect the user to the "form" class, where the user input it's choices
-else:
-    if len(response.json()['results']) < NB_Questions:
-        print("Sorry, there are only", len(response.json()['results']), "available.. Let's start anyway!")
-        NB_Questions = len(response.json()['results'])
-
-    for i in range(NB_Questions):
-        print(response.json()['results'][i]['question'])
-        list_answers = []
-        list_answers.append(response.json()['results'][i]['correct_answer'])
-        for j in response.json()['results'][i]['incorrect_answers']:
-            list_answers.append(j)
-        list_answers = random.sample(list_answers, len(list_answers))
-
-
-        #4 options if multiple choice
-        if int(Chosen_type) == 2:
-            keys_dict_multiple = [1, 2, 3, 4]
-            questions_dict = {1: str(list_answers[0]), 2: str(list_answers[1]), 3: str(list_answers[2]), 4: str(list_answers[3])}
-
-        #2 options if True/False
-        elif int(Chosen_type) == 1:
-            keys_dict_boolean = [1, 2]
-            questions_dict = {1: str(list_answers[0]), 2: str(list_answers[1])}
-
-
-        for question in questions_dict:
-            print(question, questions_dict[question])
-
-        answered = input("Input the number corresponding to the answer: ")
-
-        if questions_dict[int(answered)] == response.json()['results'][i]['correct_answer']:
-            print("correct answer")
-            Score += 1
-            print("Current Score: ", Score,"/",(i+1))
-        else:
-            print("Wrong answer, the correct asnwer was: ", response.json()['results'][i]['correct_answer'])
-            print("Current Score: ", Score, "/", (i + 1))
-
-
-    print("\n")
-    print("You final score is:", Score,"/",(NB_Questions), "or", (Score/NB_Questions)*100,"%" )
-"""
-
-
-
- 
+# NOTE: could wrap the whole program to allow to start another quiz after this round
+# NOTE: additional features could be a score board, ...
