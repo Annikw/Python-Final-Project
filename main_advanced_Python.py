16114cc4ff16be62cc2121f0a9663ca3067ba2a8
psuedo14 <sudhanshu78965@gmail.com>
2022-07-05 09:34:35 +0200

    solving the buffer problem

--- main_advanced_Python.py
+++ main_advanced_Python.py
@@ -1,94 +1,75 @@
 """
-
-ARCHITECTURE
-*   The program allows users to answer trivia questions which are tailored to the user's preferences. Users are able to choose between different categories and the number of answers that they want to get 
-    The user interface creates a simple way for the user to give their choices on which an API will be retrieved. The choices will be saved in variables and the choice of category will be cross-checked with a dictionary that contains all the different categories to ensure that users do not pick any category that doesnt exist. 
-*   Questions are retrieved from an open API: https://opentdb.com/ 
-*   The difficulty of the questions will adapt on the user's skills (questions will get easier when questions are answered wrongly and the other way around) 
-*   Creating the 'Quiz' class allows us to save all the different questions which will be asked in one specific Quiz. It also allows us to give different functionalities into the program by integrating different functions 
-*   The functionalities of the program are summarized here: 
-*   Tailored to user preferences 
-*   Adaptable difficulty level 
-*   Score tracking 
-*   Scores in the form of questions answered correctly out of total questions are tracked throughout the game 
-*   At the end of each game, the final score can be seen by the user 
-*   Restarting program 
-    *   Users can restart the quiz after the quiz ends so that there is no limit in the number of times the user can practise 
-    
-TECHNICAL SPECIFICATIONS
-*  The class Quiz has the attributes number_questions_total, q_list_easy, q_list_medium, q_list_hard. It saves the different questions which are part of the quiz and divides it by level of difficulty. The class Quiz includes the most essential functions for the user to effectively interact with the Quiz. The functions include: 
-    *  next_question_easy 
-    *  next_question_medium 
-    *  next_question_hard 
-    *  remaining questions
-    *  checking the answer
-*  The class docstring defines its attributes in more detail. Therefore, please find further specifications in the code below. 
-
-LICENSE: MIT LICENSE
-
-Copyright (c) 2022 Annikw
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-DIRECTORY
-1. OOP based approach for the class Quiz 
-2. Function to run the quiz 
-3. Designing the User interaction: choosing the category & number of questions
-4. Cleaning data & Retrieving data from API based on user's choices 
-5. Running and restarting the Quiz 
+Architecture/execution model
+*   The program allows users to answer trivia questions which are tailored to the user's preferences. Users are able to choose between different
+    categories and the number of answers that they want to get.
+*   The difficulty of the questions will adapt on the user's skills (questions will get easier when questions are answered wrong and the other way around)
+*   Dictionaries are created which save all possible categories from which the user can choose from for the quiz. This allows us to cross-check whether the specifications
+    given by the users are feasible.
+*   The user interface creates a simple way for the user to give their choices on which an API will be retrieved. The choices will be saved in variables.
+*   Questions are retrieved from an open API: https://opentdb.com/
+*   The class 'Questions' is created in which question details and content can be saved as object and to be able to further use these objects to create quizzes
+*   Creating the 'Quiz' class allows us to integrate different functions such as checking the answers.
+*   A user interface is created to allow our users to practise their trivia skills. The program includes tools such as a score taking which allows
+    the user to track and improve their performance.
+    Furthermore, the user will have the chance to restart the program right after the quiz ends so that there is no limit in the amount of times
+    the user can practise. Lastly, a timer is included for the user to measure and increase the speed in which they answer questions.
+Technical specifications
+*   The class Question has the attributes category, q_type, difficulty, question, correct_answer, incorrect_answers. It saves all information about each
+    specific question.
+*   The class Quiz has the attributes number_questions_total, q_list_easy, q_list_medium, q_list_hard. It saves the different questions which are part of the quiz
+    and divides it by level of difficulty. The class Quiz includes the most essential functions for the user to effectively interact with the Quiz. The functions include:
+    next_question_easy, next_question_medium, next_question_hard, remaining questions, and checking the answer.
+Directory
+1. Creating dictionaries for Question settings
+2. Designing the User interaction: choosing the category & number of questions
+3. Cleaning data & Retrieving data from API based on user's choices
+4. OOP based approach for Questions and Quiz
+5. Running the Quiz
 """
+
 from unicodedata import category
 import random
 import requests
 
 
-# --------------------------------OOP based approach for Questions and Quiz---------------------------------
-class Quiz:
-    """
-    The Quiz class contains information about each quiz which is created for the User. It contains all the questions of the quiz.
-    Attributes
-    ----------
-    number_questions_total: int
-        Indicates the amount of questions that the user wants to answer.
-    q_list_easy: list
-        Indicates all the questions of difficulty 'easy' in form of a list of objects of the class Questions
-    q_list_medium: list
-        Indicates all the questions of difficulty 'medium' in form of a list of objects of the class Questions
-    q_list_hard: list
-        Indicates all the questions of difficulty 'hard' in form of a list of objects of the class Questions""" 
 
-    def __init__(self, number_questions_total: int, q_list_easy: list, q_list_medium: list, q_list_hard: list):
+# --------------------------------OOP based approach for Questions and Quiz---------------------------------
+'''
+class Question:
+    def __init__(self, category, q_type, difficulty, question, correct_answer, incorrect_answers):
+        self.category = category
+        self.type = q_type
+        self.difficulty = difficulty
+        self.question = question
+        self.correct_answer = correct_answer
+        self.incorrect_answers = incorrect_answers
+
+'''
+
+
+
+
+class Quiz:
+    def __init__(self, number_questions_total, q_list_easy, q_list_medium, q_list_hard):
 
         self.number_questions_total = number_questions_total
         self.question_score = 0
-        
+
+        # NOTE: use a dictionary here (e.g. self.questions["easy"], ...)
         self.question_list_easy = q_list_easy  # list of easy questions
         self.question_list_medium = q_list_medium
         self.question_list_hard = q_list_hard
 
+        # NOTE: use dictionary here, could even combine with above
         self.q_num_easy = 0  # counter for easy questions
         self.q_num_medium = 0
         self.q_num_hard = 0
 
         self.difficulty_level = 1  # 1 for easy, 2 for medium and 3 for hard
-    # NOTE: for future --> use dictionaries to combine functions
-    
+
+
+
+    # NOTE: if you use a dictionary it would be easy to combine all the following three methods into one and providing "easy"/"medium"/"difficult" as a key to the method
     def next_question_easy(self):
         try:
             current_question = question_list_easy[self.q_num_easy]
@@ -106,6 +87,7 @@ class Quiz:
             if self.check_answer(user_answer, current_question[4]):
                 self.difficulty_level = 2
         except IndexError:
+            # NOTE: if the level 2 represents medium than you could represent this using e.g. an enum to make it easy to read
             self.difficulty_level = 2  # redirect to medium difficulty if we don't have any easy questions in dataset
 
     def next_question_medium(self):
@@ -122,6 +104,7 @@ class Quiz:
                 else:
                     print("please, answer True or False")
 
+
             if self.check_answer(user_answer, current_question[4]):
                 self.difficulty_level = 3
             else:
@@ -134,6 +117,7 @@ class Quiz:
             current_question = question_list_hard[self.q_num_hard]
             print("Difficulty of the Question:", current_question[2], ":")
             self.q_num_hard += 1
+
 
             while True:
                 user_answer = input(
@@ -151,7 +135,7 @@ class Quiz:
             self.difficulty_level = 1
 
     def remaining_questions(self):
-        return (self.q_num_easy + self.q_num_medium + self.q_num_hard) < self.number_questions_total
+        return ((self.q_num_easy + self.q_num_medium + self.q_num_hard) < self.number_questions_total)
 
     def check_answer(self, user_answer, answer):
         if user_answer.lower() == answer.lower():
@@ -167,11 +151,13 @@ class Quiz:
                 f"Your current score is: {self.question_score}/{self.q_num_easy + self.q_num_medium + self.q_num_hard}\n")
             return False
 
-        # ------------------------- Function to run the quiz ---------------------------------
+        # ------------------------- Running the Quiz ---------------------------------
+
 
     def run_quiz(self):
         self.quiz = Quiz(NB_Questions, question_list_easy, question_list_medium, question_list_hard)
         while self.quiz.remaining_questions():
+            # NOTE: see above this could be a single method quiz.next_question as the state of difficulty level is already kept by quiz and does not need to be exposed here
             if self.quiz.difficulty_level == 1:
                 self.quiz.next_question_easy()
             elif self.quiz.difficulty_level == 2:
@@ -183,10 +169,9 @@ class Quiz:
 
 # code will start from here------------------
 if __name__ == "__main__":
-    
-    # ----------------Designing the User interaction: choosing the category & number of questions-----------------
+    # ----------------Creating dictionaries for Question settings-------------------------
+
     def Chosen_Category():
-        # -----CATEGORY-----
         Categories = {0: "Random", 9: 'General Knowledge', 10: 'Entertainment: Books', 11: 'Entertainment: Film',
                       12: 'Entertainment: Music',
                       13: 'Entertainment: Musicals & Theatres', 14: 'Entertainment: Television',
@@ -198,15 +183,20 @@ if __name__ == "__main__":
                       30: 'Science: Gadgets', 31: 'Entertainment: Japanese Anime & Manga',
                       32: 'Entertainment: Cartoon & Animations'}
 
+        # ----------------Designing the User interaction: choosing the category-----------------
         print("Hi, Welcome!")
         print("Here is the Menu:")
-
+        # -----CATEGORY-----
+        # Print the number and the category
         for i in Categories:
             print(i, Categories[i])
+        # NOTE: can get rid of this variable and instead use a while true: with a break
         check_loop = True
         while check_loop:
             # Input Category
+            # NOTE: use snake case (lower case beginning)
             Chose_Cat = 0
+            # NOTE: validate that the user input is actually a number and recover if not
             Chosen_Cat = int(input("Please, input here a (valid) number for a category: "))
             # Check valid category
             try:
@@ -214,12 +204,14 @@ if __name__ == "__main__":
                 check_loop = False
             except:
                 check_loop = True
+
         print('\n')
         return Chosen_Cat
 
-    def Chosen_NB_Questions():
 
+    def Chosen_NB_Questions():
         # -----NB QUESTIONS-----
+        # NOTE: better variable naming and move this all into a function
         NB_Questions = 0
         # Check valid input
         while NB_Questions < 1 or NB_Questions > 50:
@@ -228,7 +220,9 @@ if __name__ == "__main__":
         print('\n')
         return NB_Questions
 
+
         # ----------------Cleaning data & Retrieving data from API based on user's choices-------------------------
+        # Data cleaning
     def replace_all(text, dic):
         for i, j in dic.items():
             text = text.replace(i, j)
@@ -259,6 +253,7 @@ if __name__ == "__main__":
                     pass
         return dataset
 
+
     def get_Questions(url1,url2,url3,buffer, NB_Questions, Chosen_Cat):
 
         NB_Questions_Available = NB_Questions + 1 + buffer
@@ -280,8 +275,7 @@ if __name__ == "__main__":
 
         if len(response.json()['results']) < NB_Questions:
             print("sorry, it appeears that only",len(response.json()['results']) ,"questions were available! let's start anyway!")
-
-        NB_Questions = len(response.json()['results'])
+            NB_Questions = len(response.json()['results'])
         response_json = clean_dataset(response.json())
 
         question_list_easy = []
@@ -300,9 +294,10 @@ if __name__ == "__main__":
             else:
                 question_list_hard.append(new_question)
 
+        #print(question_list_easy, question_list_medium, question_list_hard)
+
         return NB_Questions, question_list_easy, question_list_medium, question_list_hard
 
-        # ----------------Running and restarting the Quiz-------------------------
 
     restart = True
     check = True
@@ -317,6 +312,8 @@ if __name__ == "__main__":
         NB_Questions, question_list_easy, question_list_medium, question_list_hard = get_Questions(url1,url2,url3,buffer, NB_Questions, Chosen_Cat)
         Quiz(NB_Questions,question_list_easy,question_list_medium,question_list_hard).run_quiz()
 
+
+        #url = "https://opentdb.com/api.php?amount=10&category=18&type=boolean
         while check:
             play_again = input("Would you like to play again? (Yes/No) ")
             if play_again == "No" or play_again == "Yes":
@@ -328,3 +325,14 @@ if __name__ == "__main__":
         else:
             restart = False
 
+
+
+
+    #print(question_list_easy[0])
+# NOTE: could wrap the whole program to allow to start another quiz after this round
+# @Sudanshu could you look into this?
+
+# NOTE: add additional features: a timer? Showing the level of difficulty when asking a question? Something else?
+# NOTE: Wrap code in a function and call it using if name == 'main'
+# NOTE: Move classes to separate files
+# @Sudanshu
